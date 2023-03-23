09480382997116c6804f15bbed54748bf2cb571d
abnae <ab.naemi@gmail.com>
2023-03-23 10:31:33 +0100

    1 Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import React, { useState } from "react";
 function App() {
     const [count, setCount] = useState(0);
 
-    const increaseCount = () => setCount<number>(count + 1)
-    const decreaseCount = () => setCount<number>(count - 1)
+    const increaseCount = () => setCount(count + 1);
+    const decreaseCount = () => setCount(count - 1);
 
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={increaseCount}>
-                Add one Number
-            </button>
-            <button onClick={decreaseCount}>
-                Remove one Number
-            </button>
+            <div>
+                <p>You clicked {count} times</p>
+            </div>
+            <div>
+                <button onClick={increaseCount}>Add one Number</button>
+                <button onClick={decreaseCount}>Remove one Number</button>
+            </div>
         </div>
     );
 }
+
 export default App;
